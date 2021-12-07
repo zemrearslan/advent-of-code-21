a0132e291e71deac6946d20a26e2a8646ca56d0b
Zemre Arslan <za@kialo.com>
2021-12-07 23:16:44 +0100

    Day 7

---
+++ day-7/day-7-part-1.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+
+fs.readFile('/Users/zemre/Desktop/advent-of-code-wip/aoc', 'utf8' , (err, data) => {
+  if (err) {
+    console.error("hello", err);
+    return;
+  }
+
+  var arr = data.split(",").map((i) => parseInt(i));
+
+  arr.sort((a,b) => a-b);
+  let medianIndex;
+  if (arr.length % 2 == 0) {
+    var index = arr.length / 2;
+    medianIndex = arr[index] > arr[index+1] ? index+1 : index;
+  } else {
+    medianIndex = Math.ceil(arr.length/2);
+  }
+
+  var targetCrab = arr[medianIndex];
+
+  var result = arr.map(function(i) {
+    var distance = targetCrab - i;
+    var sign = Math.sign(targetCrab - i);
+    return sign >=0 ? distance : (-1)*distance;
+  }).reduce((a,b) => a+b);
+
+  console.log(result);
+});
---
+++ day-7/day-7-part-2.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+
+fs.readFile('/Users/zemre/Desktop/advent-of-code-wip/aoc', 'utf8' , (err, data) => {
+  if (err) {
+    console.error("hello", err);
+    return;
+  }
+
+  let arr = data.split(",").map((i) => parseInt(i));
+
+  arr.sort((a,b) => a-b);
+  
+  let result = 0;
+  for (let i =0; i <= arr[arr.length-1]; i++) {
+    let targetCrab = i;
+    let total = 0;
+
+    for (let k =0; k<arr.length; k++) {
+      var distance = targetCrab - arr[k];
+      var sign = Math.sign(distance);
+      var positiveDistance = sign >=0 ? distance : (-1)*distance;
+      total = total + (positiveDistance * (positiveDistance + 1) / 2);
+    }
+
+    if (i == 0 || total < result) {
+      result = total;
+    }
+  }
+
+  console.log(result);
+});
